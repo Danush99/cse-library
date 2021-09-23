75026b772e54f66c8785e0a8ed4feba92f00866b
your_username <your_email_address@example.com>
2021-09-23 11:44:31 +0530

    cseworkshop sumofintegers role A

--- src/main/java/it/uom/cse/MathOperation.java
+++ src/main/java/it/uom/cse/MathOperation.java
@@ -17,11 +17,11 @@ public class MathOperation {
     }
 
     public static int sumOfIntegers(int n){
-        int sum =0 ;
+        int summ =0 ;
         for(int i=1; i<n+1 ; i++){
-            sum+=i;
+            summ+=i;
         }
-        return sum;
+        return summ;
     }
 
 }
